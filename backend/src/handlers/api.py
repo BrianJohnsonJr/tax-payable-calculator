feeba0720bccda78e87a14299d0db7d04e9905f2
Brian Johnson <bj0020538@gmail.com>
2026-06-07 20:39:20 -0400

    Fix put error

--- backend/src/handlers/api.py
+++ backend/src/handlers/api.py
@@ -2,8 +2,9 @@ import os
 import boto3
 from uuid import uuid4
 from api.invoice_api import InvoiceApi
+from botocore.config import Config
 
-s3 = boto3.client("s3")
+s3 = boto3.client("s3", region_name=os.environ["AWS_REGION"], config=Config(signature_version="s3v4"))
 dynamodb = boto3.resource("dynamodb")
 invoices_table = dynamodb.Table(os.environ["INVOICES_TABLE"])
 bucket = os.environ['INVOICE_BUCKET']
